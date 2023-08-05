8cc6a1e04e06acc5fe658193abb4881b55dac701
Dan Herbert <http-errs-dan@hrbrt.co>
2023-08-05 21:55:43 +0000

    Upgrade to pug3 & do version checks in publish script
    Also allow the publish script to run on devices that don't have the
    exact tools installed by using npx to load them on the fly if possible.

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 echo 'Beware of new files. They will not automatically be compiled'
-sleep 3
+sleep 2
 
 # Setup stylus watcher, and inline images as data URIs
 stylus --watch --inline --out ./css/ styl/ &
 # Setup pug templates
-pug --pretty --watch --out ./html ./pug &
+pug3 --pretty --watch --out ./html ./pug &
 # Keep the script running until something crashes or Ctrl+C
 wait
-
