f832f30ee47eef4321d137db08f81566f307509f
Dan Herbert <http-errs-dan@hrbrt.co>
2023-08-06 17:48:37 +0000

    DRY-er scripts for building the html files.
    Commands for pug/stylus have been reduced to a single location
    regardless of whether running natively or through npx so that flags are
    harder to be forgotten.
    
    Also get rid of the version checks for both tools to keep things more
    readable.

--- publish.sh
+++ publish.sh
@@ -2,62 +2,28 @@
 # NOTE: This script only generates the html files in a production-ready format.
 # It does not copy them over to their final destination, wherever that may be.
 
-vercomp () {
-    if [[ $1 == $2 ]]
-    then
-        echo -n 0
-        return
-    fi
-    local IFS=.
-    local i ver1=($1) ver2=($2)
-    # fill empty fields in ver1 with zeros
-    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
-    do
-        ver1[i]=0
-    done
-    for ((i=0; i<${#ver1[@]}; i++))
-    do
-        if [[ -z ${ver2[i]} ]]
-        then
-            # fill empty fields in ver2 with zeros
-            ver2[i]=0
-        fi
-        if ((10#${ver1[i]} > 10#${ver2[i]}))
-        then
-            echo -n 1
-            return
-        fi
-        if ((10#${ver1[i]} < 10#${ver2[i]}))
-        then
-            echo -n 2
-            return
-        fi
-    done
-    echo -n 0
-    return
-}
+shopt -s extglob
 
 set -eux
 
 # Build the CSS
-# Older versions of stylus have base64 encoding problems that require recent
-# versions of the app to encode things properly.
-if command -v stylus &> /dev/null && [[ $(vercomp "$(stylus --version 2>&1)" '0.59.0') -ge '0' ]]; then
-    stylus --compress --inline --out ./css/ styl/
+stylus_cmd="stylus --compress --inline --out ./css/ styl/"
+if ! command -v stylus &> /dev/null; then
+    npx --package 'stylus' "$stylus_cmd"
 else
-    npx 'stylus@>=0.59.0' --compress --inline --out ./css/ styl/
+    $stylus_cmd
 fi
 
 # Build the HTML, which inlines the CSS
+# Globbing is required for the templates path, hence the shellcheck disablings.
+pug_cmd="pug3 --out ./html ./pug/!(*-base).pug"
 if ! command -v pug3 &> /dev/null; then
-    # Unfortunately, there's no easy way to compare pre-release semvers
-    npx --package '@anduh/pug-cli@>=1.0.0-alpha8' pug3 --out ./html ./pug
+    # shellcheck disable=SC2086
+    npx --package '@anduh/pug-cli' $pug_cmd
 else
-    pug3 --out ./html ./pug
+    # shellcheck disable=SC2086
+    $pug_cmd
 fi
 
-# Get rid of the base template
-rm ./html/error-base.html
-
-# Rename a class for index so it makes more sense.
+# Rename a class for index so it makes more semantic sense.
 sed -i -- 's/error-name/page-name/g' ./html/index.html
--- run.sh
+++ run.sh
@@ -2,9 +2,25 @@
 echo 'Beware of new files. They will not automatically be compiled'
 sleep 2
 
+shopt -s extglob
+
 # Setup stylus watcher, and inline images as data URIs
-stylus --watch --inline --out ./css/ styl/ &
-# Setup pug templates
-pug3 --pretty --watch --out ./html ./pug &
+stylus_cmd="stylus --watch --inline --out ./css/ styl/ &"
+if ! command -v stylus &> /dev/null; then
+    npx --package 'stylus' "$stylus_cmd"
+else
+    "$stylus_cmd"
+fi
+
+# Globbing is required for the templates path, hence the shellcheck disablings.
+pug_cmd="pug3 --pretty --watch --out ./html ./pug/!(*-base).pug &"
+if ! command -v pug3 &> /dev/null; then
+    # shellcheck disable=SC2086
+    npx --package '@anduh/pug-cli' $pug_cmd
+else
+    # shellcheck disable=SC2086
+    $pug_cmd
+fi
+
 # Keep the script running until something crashes or Ctrl+C
 wait
